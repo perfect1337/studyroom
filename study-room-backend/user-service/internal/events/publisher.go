9596d4cfe40220b44900f8fdadd1e931391679f1
unknown <mrdengrif@gmail.com>
2026-09-07 20:45:21 +0400

    MULTIFILIALIONIO

--- study-room-backend/user-service/internal/events/publisher.go
+++ study-room-backend/user-service/internal/events/publisher.go
@@ -126,6 +126,7 @@ type UserDeletedEvent struct {
 	LastName  string  `json:"last_name"`
 	Role      string  `json:"role"`
 	BranchID  *int64  `json:"branch_id,omitempty"`
+	BranchIDs []int64  `json:"branch_ids,omitempty"`
 	ParentIDs []int64 `json:"parent_ids,omitempty"`
 }
 
